rostedt/shelf

commit 221ca41abb36be87ce16a6205b4014026de158a1

shelf: Initial commit of the Shell ELF parser
This is the initial commit of the code to create a shell command line to
read ELF files.

Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

src/dump.c
@@ -1,0 +1,254 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2022 Google Inc, Steven Rostedt <rostedt@goodmis.org>
+ */
+#include <regex.h>
+#include "shelf.h"
+
+static void dump_usage(struct ccli *ccli)
+{
+	ccli_printf(ccli, "usage: dump section <start-addr>[ - <end-addr>| <size>]\n"
+		    "usage: dump symbol <start-symbol>[ - <end-symbol>| <size>]\n");
+}
+
+static int dump_symbol_usage(struct ccli *ccli)
+{
+	ccli_printf(ccli, "usage: dump symbol <start-symbol>[ - <end-symbol>| <size>]\n");
+	return 0;
+}
+
+static union Elf_Shdr *find_section(struct shelf *shelf, const char *sec)
+{
+	union Elf_Shdr *shdr;
+	const char *name;
+	int i;
+
+	for (i = 0; i < shelf->shnum; i++) {
+		shdr = get_shdr(shelf, i);
+		name = shdr_name(shelf, shdr);
+		if (name && strcmp(name, sec) == 0)
+			return shdr;
+	}
+	return NULL;
+}
+
+static int dump_section(struct ccli *ccli, void *data,
+			int argc, char **argv)
+{
+	struct shelf *shelf = data;
+	union Elf_Shdr *shdr;
+
+	shdr = find_section(shelf, argv[0]);
+	if (!shdr)
+		ccli_printf(ccli, "Section '%s' not found\n", argv[0]);
+
+	return 0;
+}
+
+static union Elf_Sym *find_symbol(struct shelf *shelf, const char *sec)
+{
+	union Elf_Sym *sym;
+	const char *name;
+	int i;
+
+	for (i = 0; i < shelf->symnum; i++) {
+		sym = get_sym(shelf, i);
+		name = sym_name(shelf, sym);
+		if (name && strcmp(name, sec) == 0)
+			return sym;
+	}
+	return NULL;
+}
+
+static int print_addr(struct ccli *ccli, struct shelf *shelf,
+		       uint64_t addr, uint64_t offset, int line)
+{
+	uint64_t val64;
+	uint32_t val32;
+
+	if (shelf->sixtyfour) {
+		val64 = *((uint64_t *)(shelf->map + offset));
+		return ccli_page(ccli, line, "%16zx:\t%16zx\n", addr, val64);
+	} else {
+		val32 = *((uint32_t *)(shelf->map + offset));
+		return ccli_page(ccli, line, "%8zx:\t%8x\n", addr, val32);
+	}
+}
+
+static int dump_symbol(struct ccli *ccli, void *data,
+		       int argc, char **argv)
+{
+	struct shelf *shelf = data;
+	union Elf_Sym *sym;
+	union Elf_Shdr *shdr;
+	uint64_t start, stop;
+	uint64_t addr, offset;
+	uint64_t size;
+	uint16_t shndx;
+	int line = 1;
+	int inc;
+
+	if (argc < 1)
+		return dump_symbol_usage(ccli);
+
+	sym = find_symbol(shelf, argv[0]);
+	if (!sym)
+		ccli_printf(ccli, "Symbol '%s' not found\n", argv[0]);
+
+	shndx = sym_shndx(shelf, sym);
+	shdr = get_shdr(shelf, shndx);
+	addr = shdr_addr(shelf, shdr);
+	size = shdr_size(shelf, shdr);
+	start = shdr_offset(shelf, shdr);
+
+	offset = sym_value(shelf, sym);
+
+	if (offset < addr || offset >= addr + size) {
+		ccli_printf(ccli, "# sym %s offset %zx not in range %zx-%zx\n",
+			    argv[0], offset, addr, addr + size);
+		return 0;
+	}
+
+	start += offset - addr;
+
+	if (argc < 2) {
+		print_addr(ccli, shelf, addr, start, 1);
+		return 0;
+	}
+
+	if (strcmp(argv[1], "-") == 0) {
+		if (argc < 3)
+			return dump_symbol_usage(ccli);
+
+		sym = find_symbol(shelf, argv[2]);
+		if (!sym)
+			ccli_printf(ccli, "Symbol '%s' not found\n", argv[2]);
+
+
+		shndx = sym_shndx(shelf, sym);
+		if (shdr != get_shdr(shelf, shndx)) {
+			ccli_printf(ccli, "Symbol '%s' in section '%s' not in same section\n"
+				    "  as symbol '%s' in section '%s'\n",
+				    argv[0], shdr_name(shelf, shdr),
+				    argv[2], shdr_name(shelf, get_shdr(shelf, shndx)));
+			return 0;
+		}
+
+		stop = start + sym_value(shelf, sym) - offset;
+		if (stop < start) {
+			ccli_printf(ccli, "Symbol '%s' is before symbol '%s'\n",
+				    argv[2], argv[0]);
+			return 0;
+		}
+
+		size = stop - start;
+	} else {
+		size = strtoll(argv[1], NULL, 0);
+	}
+
+	inc = shelf->sixtyfour ? 8 : 4;
+
+	while (size > 0) {
+		line = print_addr(ccli, shelf, addr, start, line);
+		if (line < 0)
+			break;
+		addr += inc;
+		start += inc;
+		if (size < inc)
+			break;
+		size -= inc;
+	}
+
+	return 0;
+}
+
+int dump_cmd(struct ccli *ccli, const char *command, const char *line,
+	     void *data, int argc, char **argv)
+{
+	if (argc < 2) {
+		dump_usage(ccli);
+		return 0;
+	}
+
+	if (strcmp(argv[1], "section") == 0)
+		return dump_section(ccli, data, argc - 2, argv + 2);
+
+	if (strcmp(argv[1], "symbol") == 0)
+		return dump_symbol(ccli, data, argc - 2, argv + 2);
+
+	return 0;
+}
+
+static int dump_section_completion(struct ccli *ccli, void *data,
+				   int argc, char **argv,
+				   char ***list, int word, char *match)
+{
+	if (!argc || (argc == 1 && strlen(match)))
+		return section_completion(ccli, data, list, word);
+
+	return 0;
+}
+
+static int dump_symbol_completion(struct ccli *ccli, void *data,
+				  int argc, char **argv,
+				  char ***list, int word, char *match)
+{
+	char **words;
+
+	if (!argc || (argc == 1 && strlen(match)))
+		return symbol_completion(ccli, data, list, word);
+
+	if (argc == 1 && !strlen(match)) {
+		words = calloc(1, sizeof(*words));
+		if (!words)
+			return 0;
+		words[0] = strdup("-");
+		*list = words;
+		return 1;
+	}
+
+	if (argc == 2 || (argc == 3 && strlen(match))) {
+		if (strcmp(argv[1], "-") == 0)
+			return symbol_completion(ccli, data, list, word);
+	}
+
+	return 0;
+}
+
+int dump_completion(struct ccli *ccli, const char *command,
+		    const char *line, int word,
+		    char *match, char ***list, void *data)
+{
+	char *types[] = { "section", "symbol" };
+	char **words;
+	char **argv;
+	int argc;
+	int ret = 0;
+	int i;
+
+	if (word == 1) {
+		words = calloc(ARRAY_SIZE(types), sizeof(char *));
+		if (!words)
+			return 0;
+		for (i = 0; i < ARRAY_SIZE(types); i++)
+			words[i] = strdup(types[i]);
+		*list = words;
+		return i;
+	}
+
+	argc = ccli_line_parse(line, &argv);
+	if (argc < 0)
+		return 0;
+
+	if (strcmp(argv[1], "section") == 0)
+		ret = dump_section_completion(ccli, data, argc - 2, argv + 2,
+					      list, word - 2, match);
+
+	if (strcmp(argv[1], "symbol") == 0)
+		ret = dump_symbol_completion(ccli, data, argc - 2, argv + 2,
+					list, word - 2, match);
+
+	ccli_argv_free(argv);
+
+	return ret;
+}

.gitignore
@@ -50,3 +50,6 @@ modules.order
 Module.symvers
 Mkfile.old
 dkms.conf
+
+#Editors
+*~

LICENSE
@@ -1,12 +1,31 @@
-                    GNU GENERAL PUBLIC LICENSE
-                       Version 2, June 1991
+Valid-License-Identifier: GPL-2.0
+Valid-License-Identifier: GPL-2.0-only
+Valid-License-Identifier: GPL-2.0+
+Valid-License-Identifier: GPL-2.0-or-later
+SPDX-URL: https://spdx.org/licenses/GPL-2.0.html
+Usage-Guide:
+  To use this license in source code, put one of the following SPDX
+  tag/value pairs into a comment according to the placement
+  guidelines in the licensing rules documentation.
+  For 'GNU General Public License (GPL) version 2 only' use:
+    SPDX-License-Identifier: GPL-2.0
+  or
+    SPDX-License-Identifier: GPL-2.0-only
+  For 'GNU General Public License (GPL) version 2 or any later version' use:
+    SPDX-License-Identifier: GPL-2.0+
+  or
+    SPDX-License-Identifier: GPL-2.0-or-later
+License-Text:
 
- Copyright (C) 1989, 1991 Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+		    GNU GENERAL PUBLIC LICENSE
+		       Version 2, June 1991
+
+ Copyright (C) 1989, 1991 Free Software Foundation, Inc.
+                       51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  Everyone is permitted to copy and distribute verbatim copies
  of this license document, but changing it is not allowed.
 
-                            Preamble
+			    Preamble
 
   The licenses for most software are designed to take away your
 freedom to share and change it.  By contrast, the GNU General Public
@@ -15,7 +34,7 @@ software--to make sure the software is free for all its users.  This
 General Public License applies to most of the Free Software
 Foundation's software and to any other program whose authors commit to
 using it.  (Some other Free Software Foundation software is covered by
-the GNU Lesser General Public License instead.)  You can apply it to
+the GNU Library General Public License instead.)  You can apply it to
 your programs, too.
 
   When we speak of free software, we are referring to freedom, not
@@ -55,8 +74,8 @@ patent must be licensed for everyone's free use or not licensed at all.
 
   The precise terms and conditions for copying, distribution and
 modification follow.
-
-                    GNU GENERAL PUBLIC LICENSE
+
+		    GNU GENERAL PUBLIC LICENSE
    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
   0. This License applies to any program or other work which contains
@@ -110,7 +129,7 @@ above, provided that you also meet all of these conditions:
     License.  (Exception: if the Program itself is interactive but
     does not normally print such an announcement, your work based on
     the Program is not required to print an announcement.)
-
+
 These requirements apply to the modified work as a whole.  If
 identifiable sections of that work are not derived from the Program,
 and can be reasonably considered independent and separate works in
@@ -168,7 +187,7 @@ access to copy from a designated place, then offering equivalent
 access to copy the source code from the same place counts as
 distribution of the source code, even though third parties are not
 compelled to copy the source along with the object code.
-
+
   4. You may not copy, modify, sublicense, or distribute the Program
 except as expressly provided under this License.  Any attempt
 otherwise to copy, modify, sublicense or distribute the Program is
@@ -225,7 +244,7 @@ impose that choice.
 
 This section is intended to make thoroughly clear what is believed to
 be a consequence of the rest of this License.
-
+
   8. If the distribution and/or use of the Program is restricted in
 certain countries either by patents or by copyrighted interfaces, the
 original copyright holder who places the Program under this License
@@ -255,7 +274,7 @@ make exceptions for this.  Our decision will be guided by the two goals
 of preserving the free status of all derivatives of our free software and
 of promoting the sharing and reuse of software generally.
 
-                            NO WARRANTY
+			    NO WARRANTY
 
   11. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
 FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.  EXCEPT WHEN
@@ -277,9 +296,9 @@ YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER
 PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGES.
 
-                     END OF TERMS AND CONDITIONS
-
-            How to Apply These Terms to Your New Programs
+		     END OF TERMS AND CONDITIONS
+
+	    How to Apply These Terms to Your New Programs
 
   If you develop a new program, and you want it to be of the greatest
 possible use to the public, the best way to achieve this is to make it
@@ -303,9 +322,10 @@ the "copyright" line and a pointer to where the full notice is found.
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 Also add information on how to contact you by electronic and paper mail.
 
@@ -335,5 +355,5 @@ necessary.  Here is a sample; alter the names:
 This General Public License does not permit incorporating your program into
 proprietary programs.  If your program is a subroutine library, you may
 consider it more useful to permit linking proprietary applications with the
-library.  If this is what you want to do, use the GNU Lesser General
+library.  If this is what you want to do, use the GNU Library General
 Public License instead of this License.

Makefile
@@ -1,0 +1,259 @@
+# SPDX-License-Identifier: LGPL-2.1
+# shelf version:
+SE_VERSION = 0
+SE_PATCHLEVEL = 0
+SE_EXTRAVERSION = dev
+SHELF_VERSION = $(SE_VERSION).$(SE_PATCHLEVEL).$(SE_EXTRAVERSION)
+
+export SE_VERSION
+export SE_PATCHLEVEL
+export SE_EXTRAVERSION
+export SHELF_VERSION
+
+MAKEFLAGS += --no-print-directory
+
+# Makefiles suck: This macro sets a default value of $(2) for the
+# variable named by $(1), unless the variable has been set by
+# environment or command line. This is necessary for CC and AR
+# because make sets default values, so the simpler ?= approach
+# won't work as expected.
+define allow-override
+  $(if $(or $(findstring environment,$(origin $(1))),\
+            $(findstring command line,$(origin $(1)))),,\
+    $(eval $(1) = $(2)))
+endef
+
+# Allow setting CC and AR, or setting CROSS_COMPILE as a prefix.
+$(call allow-override,CC,$(CROSS_COMPILE)gcc)
+$(call allow-override,AR,$(CROSS_COMPILE)ar)
+$(call allow-override,PKG_CONFIG,pkg-config)
+$(call allow-override,LD_SO_CONF_PATH,/etc/ld.so.conf.d/)
+$(call allow-override,LDCONFIG,ldconfig)
+
+EXT = -std=gnu99
+INSTALL = install
+
+# Use DESTDIR for installing into a different root directory.
+# This is useful for building a package. The program will be
+# installed in this directory as if it was the root directory.
+# Then the build tool can move it later.
+DESTDIR ?=
+DESTDIR_SQ = '$(subst ','\'',$(DESTDIR))'
+
+LP64 := $(shell echo __LP64__ | ${CC} ${CFLAGS} -E -x c - | tail -n 1)
+ifeq ($(LP64), 1)
+  libdir_relative_temp = lib64
+else
+  libdir_relative_temp = lib
+endif
+
+libdir_relative ?= $(libdir_relative_temp)
+prefix ?= /usr/local
+man_dir = $(prefix)/share/man
+man_dir_SQ = '$(subst ','\'',$(man_dir))'
+libdir = $(prefix)/$(libdir_relative)
+libdir_SQ = '$(subst ','\'',$(libdir))'
+includedir_relative ?= include
+includedir = $(prefix)/$(includedir_relative)
+includedir_SQ = '$(subst ','\'',$(includedir))'
+pkgconfig_dir ?= $(word 1,$(shell $(PKG_CONFIG) 		\
+			--variable pc_path pkg-config | tr ":" " "))
+
+
+etcdir ?= /etc
+etcdir_SQ = '$(subst ','\'',$(etcdir))'
+
+export man_dir man_dir_SQ html_install html_install_SQ INSTALL
+export img_install img_install_SQ
+export DESTDIR DESTDIR_SQ
+
+pound := \#
+
+HELP_DIR = -DHELP_DIR=$(html_install)
+HELP_DIR_SQ = '$(subst ','\'',$(HELP_DIR))'
+#' emacs highlighting gets confused by the above escaped quote.
+
+BASH_COMPLETE_DIR ?= $(etcdir)/bash_completion.d
+
+# copy a bit from Linux kbuild
+
+ifeq ("$(origin V)", "command line")
+  VERBOSE = $(V)
+endif
+ifndef VERBOSE
+  VERBOSE = 0
+endif
+
+SILENT := $(if $(findstring s,$(filter-out --%,$(MAKEFLAGS))),1)
+
+LIBCCLI_MIN_VERSION = 1.1
+
+# Test for necessary libraries
+TEST_LIBCCLI = $(shell sh -c "$(PKG_CONFIG) --atleast-version $(LIBCCLI_MIN_VERSION) libccli > /dev/null 2>&1 && echo y")
+
+ifeq ("$(TEST_LIBCCLI)", "y")
+LIBCCLI_INCLUDES = $(shell sh -c "$(PKG_CONFIG) --cflags libccli")
+LIBCCLI_LIBS = $(shell sh -c "$(PKG_CONFIG) --libs libccli")
+else
+ ifneq ($(MAKECMDGOALS),clean)
+   $(error libccli.so minimum version of $(LIBCCLI_MIN_VERSION) not installed)
+ endif
+endif
+
+# $(call test-build, snippet, ret) -> ret if snippet compiles
+#                                  -> empty otherwise
+test-build = $(if $(shell sh -c 'echo "$(1)" | \
+	$(CC) -o /dev/null -c -x c - > /dev/null 2>&1 && echo y'), $2)
+
+ifeq ("$(origin O)", "command line")
+
+  saved-output := $(O)
+  BUILD_OUTPUT := $(shell cd $(O) && /bin/pwd)
+  $(if $(BUILD_OUTPUT),, \
+    $(error output directory "$(saved-output)" does not exist))
+
+else
+  BUILD_OUTPUT = $(CURDIR)
+endif
+
+srctree		:= $(if $(BUILD_SRC),$(BUILD_SRC),$(CURDIR))
+objtree		:= $(BUILD_OUTPUT)
+src		:= $(srctree)
+obj		:= $(objtree)
+bdir		:= $(obj)/src
+
+export prefix src obj bdir
+
+LIBS =$(LIBCCLI_LIBS)
+
+export LIBS
+
+export Q SILENT VERBOSE EXT
+
+# Include the utils
+include scripts/utils.mk
+
+# Set compile option CFLAGS if not set elsewhere
+CFLAGS ?= -g -Wall
+CPPFLAGS ?=
+LDFLAGS ?=
+
+CFLAGS += $(LIBCCLI_INCLUDES)
+
+export CFLAGS
+export INCLUDES
+
+# Append required CFLAGS
+override CFLAGS += -D_GNU_SOURCE $(INCLUDES)
+
+all: all_cmd
+
+TARGETS = shelf
+
+all_cmd: $(TARGETS)
+
+shelf: force
+	make -C src $@
+
+VERSION_FILE = ktest_version.h
+
+VIM_TAGS = $(obj)/tags
+EMACS_TAGS = $(obj)/TAGS
+CSCOPE_TAGS = $(obj)/cscope
+
+$(VIM_TAGS): force
+	$(RM) $@
+	$(call find_tag_files) | (cd $(obj) && xargs ctags --extra=+f --c-kinds=+px)
+
+$(EMACS_TAGS): force
+	$(RM) $@
+	$(call find_tag_files) | (cd $(obj) && xargs etags)
+
+$(CSCOPE_TAGS): force
+	$(RM) $(obj)/cscope*
+	$(call find_tag_files) | cscope -b -q
+
+tags: $(VIM_TAGS)
+TAGS: $(EMACS_TAGS)
+cscope: $(CSCOPE_TAGS)
+
+install:
+
+doc:
+	$(Q)$(call descend,$(src)/Documentation,all)
+
+doc_clean:
+	$(Q)$(call descend,$(src)/Documentation,clean)
+
+install_doc:
+	$(Q)$(call descend,$(src)/Documentation,install)
+
+define build_uninstall_script
+	$(Q)mkdir $(BUILD_OUTPUT)/tmp_build
+	$(Q)$(MAKE) -C $(src) DESTDIR=$(BUILD_OUTPUT)/tmp_build/ O=$(BUILD_OUTPUT) $1 > /dev/null
+	$(Q)find $(BUILD_OUTPUT)/tmp_build ! -type d -printf "%P\n" > $(BUILD_OUTPUT)/build_$2
+	$(Q)$(RM) -rf $(BUILD_OUTPUT)/tmp_build
+endef
+
+build_uninstall: $(BUILD_PREFIX)
+	$(call build_uninstall_script,install,uninstall)
+
+$(BUILD_OUTPUT)/build_uninstall: build_uninstall
+
+define uninstall_file
+	if [ -f $(DESTDIR)/$1 -o -h $(DESTDIR)/$1 ]; then \
+		$(call print_uninstall,$(DESTDIR)/$1)$(RM) $(DESTDIR)/$1; \
+	fi;
+endef
+
+uninstall: $(BUILD_OUTPUT)/build_uninstall
+	@$(foreach file,$(shell cat $(BUILD_OUTPUT)/build_uninstall),$(call uninstall_file,$(file)))
+
+PHONY += force
+force:
+
+# Declare the contents of the .PHONY variable as phony.  We keep that
+# information in a variable so we can use it in if_changed and friends.
+.PHONY: $(PHONY)
+
+OBJS := $(OBJS:%.o=$(bdir)/%.o)
+DEPS := $(OBJS:$(bdir)/%.o=$(bdir)/.%.d)
+
+all: $(DEFAULT_TARGET)
+
+$(bdir):
+	@mkdir -p $(bdir)
+
+VERSION = $(KT_VERSION)
+PATCHLEVEL = $(KT_PATCHLEVEL)
+EXTRAVERSION = $(KT_EXTRAVERSION)
+
+define make_version.h
+  (echo '/* This file is automatically generated. Do not modify. */';		\
+   echo \#define VERSION_CODE $(shell						\
+   expr $(VERSION) \* 256 + $(PATCHLEVEL));					\
+   echo '#define EXTRAVERSION ' $(EXTRAVERSION);				\
+   echo '#define VERSION_STRING "'$(VERSION).$(PATCHLEVEL).$(EXTRAVERSION)'"';	\
+  ) > $1
+endef
+
+define update_version.h
+  ($(call make_version.h, $@.tmp);		\
+    if [ -r $@ ] && cmp -s $@ $@.tmp; then	\
+      rm -f $@.tmp;				\
+    else					\
+      echo '  UPDATE             $@';		\
+      mv -f $@.tmp $@;				\
+    fi);
+endef
+
+$(VERSION_FILE): force
+	$(Q)$(call update_version.h)
+
+clean:
+	$(Q)$(call descend_clean,src)
+	$(Q)$(call do_clean, \
+	  $(TARGETS) $(bdir)/*.a $(bdir)/*.so $(bdir)/*.so.* $(bdir)/*.o $(bdir)/.*.d \
+	  $(VERSION_FILE))
+
+.PHONY: clean

scripts/utils.mk
@@ -1,0 +1,226 @@
+# SPDX-License-Identifier: GPL-2.0
+
+# Utils
+
+PWD		:= $(shell /bin/pwd)
+GOBJ		= $(notdir $(strip $@))
+BASE1		= $(notdir $(strip $1))
+BASE2		= $(notdir $(strip $2))
+BASEPWD		= $(notdir $(strip $(PWD)))
+
+ifeq ($(VERBOSE),1)
+  Q =
+  S =
+else
+  Q = @
+  S = -s
+endif
+
+# Use empty print_* macros if either SILENT or VERBOSE.
+ifeq ($(findstring 1,$(SILENT)$(VERBOSE)),1)
+  print_compile =
+  print_app_build =
+  print_fpic_compile =
+  print_shared_lib_compile =
+  print_plugin_obj_compile =
+  print_plugin_build =
+  print_install =
+  print_uninstall =
+  print_update =
+  print_descend =
+  print_clean =
+  print_asciidoc =
+  print_xsltproc =
+  print_install =
+  hide_xsltproc_output =
+else
+  print_compile =		echo '  COMPILE            '$(GOBJ);
+  print_app_build =		echo '  BUILD              '$(GOBJ);
+  print_fpic_compile =		echo '  COMPILE FPIC       '$(GOBJ);
+  print_shared_lib_compile =	echo '  COMPILE SHARED LIB '$(GOBJ);
+  print_plugin_obj_compile =	echo '  COMPILE PLUGIN OBJ '$(GOBJ);
+  print_plugin_build =		echo '  BUILD PLUGIN       '$(GOBJ);
+  print_static_lib_build =	echo '  BUILD STATIC LIB   '$(GOBJ);
+  print_install =		echo '  INSTALL     '$(GSPACE)$1'	to	$(DESTDIR_SQ)$2';
+  print_update =		echo '  UPDATE             '$(GOBJ);
+  print_descend =		echo '  DESCEND            '$(BASE1) $(BASE2);
+  print_clean =			echo '  CLEAN              '$(BASEPWD);
+  print_uninstall =		echo '  UNINSTALLING $(DESTDIR_SQ)$1';
+  print_asciidoc =		echo '  ASCIIDOC            '`basename $@`;
+  print_xsltproc =		echo '  XSLTPROC            '`basename $@`;
+  print_install =		echo '  INSTALL             '`basename $1`'	to	$(DESTDIR_SQ)'$2;
+  hide_xsltproc_output = 2> /dev/null
+endif
+
+do_fpic_compile =					\
+	($(print_fpic_compile)				\
+	$(CC) -c $(CPPFLAGS) $(CFLAGS) $(EXT) -fPIC $< -o $@)
+
+do_compile =							\
+	($(if $(GENERATE_PIC), $(do_fpic_compile),		\
+	 $(print_compile)					\
+	 $(CC) -c $(CPPFLAGS) $(CFLAGS) $(EXT) $< -o $@))
+
+do_app_build =						\
+	($(print_app_build)				\
+	$(CC) $^ -rdynamic -Wl,-rpath=$(libdir) -o $@ $(LDFLAGS) $(CONFIG_LIBS) $(LIBS))
+
+do_build_static_lib =				\
+	($(print_static_lib_build)		\
+	$(RM) $@;  $(AR) rcs $@ $^)
+
+do_compile_shared_library =			\
+	($(print_shared_lib_compile)		\
+	$(CC) --shared $^ '-Wl,-soname,$(@F),-rpath=$$ORIGIN' -o $@ $(LDFLAGS) $(LIBS))
+
+do_compile_plugin_obj =				\
+	($(print_plugin_obj_compile)		\
+	$(CC) -c $(CPPFLAGS) $(CFLAGS) -fPIC -o $@ $<)
+
+do_plugin_build =				\
+	($(print_plugin_build)			\
+	$(CC) $(CFLAGS) $(LDFLAGS) -shared -nostartfiles -o $@ $<)
+
+do_clean =					\
+	($(print_clean)				\
+	$(RM) $1)
+
+do_compile_python_plugin_obj =			\
+	($(print_plugin_obj_compile)		\
+	$(CC) -c $(CPPFLAGS) $(CFLAGS) $(PYTHON_DIR_SQ) $(PYTHON_INCLUDES) -fPIC -o $@ $<)
+
+do_python_plugin_build =			\
+	($(print_plugin_build)			\
+	$(CC) $< -shared $(LDFLAGS) $(PYTHON_LDFLAGS) -o $@)
+
+define make_version.h
+	(echo '/* This file is automatically generated. Do not modify. */';		\
+	echo \#define VERSION_CODE $(shell						\
+	expr $(VERSION) \* 256 + $(PATCHLEVEL));					\
+	echo '#define EXTRAVERSION ' $(EXTRAVERSION);					\
+	echo '#define VERSION_STRING "'$(VERSION).$(PATCHLEVEL).$(EXTRAVERSION)'"';	\
+	echo '#define FILE_VERSION '$(FILE_VERSION);					\
+	if [ -d $(src)/.git ]; then							\
+	  d=`git diff`;									\
+	  x="";										\
+	  if [ ! -z "$$d" ]; then x="+"; fi;						\
+	  echo '#define VERSION_GIT "'$(shell 						\
+		git log -1 --pretty=format:"%H" 2>/dev/null)$$x'"';			\
+	else										\
+	  echo '#define VERSION_GIT "not-a-git-repo"';					\
+	fi										\
+	) > $1
+endef
+
+define update_version.h
+	($(call make_version.h, $@.tmp);				\
+	if [ -r $@ ] && cmp -s $@ $@.tmp; then				\
+		rm -f $@.tmp;						\
+	else								\
+		$(print_update)						\
+		mv -f $@.tmp $@;					\
+	fi);
+endef
+
+define update_dir
+	(echo $1 > $@.tmp;	\
+	if [ -r $@ ] && cmp -s $@ $@.tmp; then				\
+		rm -f $@.tmp;						\
+	else								\
+		$(print_update)						\
+		mv -f $@.tmp $@;					\
+	fi);
+endef
+
+define build_prefix
+	(echo $1 > $@.tmp;	\
+	if [ -r $@ ] && cmp -s $@ $@.tmp; then				\
+		rm -f $@.tmp;						\
+	else								\
+		$(print_update)						\
+		mv -f $@.tmp $@;					\
+	fi);
+endef
+
+define do_install
+	$(print_install)				\
+	if [ ! -d '$(DESTDIR_SQ)$2' ]; then		\
+		$(INSTALL) -d -m 755 '$(DESTDIR_SQ)$2';	\
+	fi;						\
+	$(INSTALL) $(if $3,-m $3,) $1 '$(DESTDIR_SQ)$2'
+endef
+
+define do_install_data
+	$(print_install)				\
+	if [ ! -d '$(DESTDIR_SQ)$2' ]; then		\
+		$(INSTALL) -d -m 755 '$(DESTDIR_SQ)$2';	\
+	fi;						\
+	$(INSTALL) -m 644 $1 '$(DESTDIR_SQ)$2'
+endef
+
+#
+# Define a callable command for descending to a new directory
+#
+# Call by doing: $(call descend,directory[,target])
+#
+descend = \
+	($(print_descend)		\
+	mkdir -p $(obj)/$(BASE1); \
+	$(MAKE) $(PRINT_DIR) bdir=$(obj)/$(BASE1) -C $(1) $(2))
+
+descend_clean = \
+	$(MAKE) $(PRINT_DIR) bdir=$(obj)/$(BASE1) -C $(1) clean
+
+define do_install_pkgconfig_file
+	if [ -n "${pkgconfig_dir}" ]; then 					\
+		$(call do_install,$(PKG_CONFIG_FILE),$(pkgconfig_dir),644); 	\
+	else 									\
+		(echo Failed to locate pkg-config directory) 1>&2;		\
+	fi
+endef
+
+define do_make_pkgconfig_file
+	$(print_app_build)
+	$(Q)cp -f $(srctree)/${PKG_CONFIG_SOURCE_FILE}.template ${PKG_CONFIG_FILE};	\
+	sed -i "s|INSTALL_PREFIX|${1}|g" ${PKG_CONFIG_FILE}; 		\
+	sed -i "s|LIB_VERSION|${LIBTRACECMD_VERSION}|g" ${PKG_CONFIG_FILE}; \
+	sed -i "s|LIB_DIR|$(libdir)|g" ${PKG_CONFIG_FILE}; \
+	sed -i "s|LIBTRACEFS_MIN_VERSION|$(LIBTRACEFS_MIN_VERSION)|g" ${PKG_CONFIG_FILE}; \
+	sed -i "s|HEADER_DIR|$(includedir)/trace-cmd|g" ${PKG_CONFIG_FILE};
+endef
+
+do_asciidoc_build =				\
+	($(print_asciidoc)			\
+	 asciidoc -d manpage -b docbook -o $@ $<)
+
+do_xsltproc_build =				\
+	($(print_xsltproc)			\
+	 xsltproc --nonet -o $@ ${MANPAGE_DOCBOOK_XSL} $< $(hide_xsltproc_output))
+
+#
+# asciidoc requires a synopsis, but file format man pages (5) do
+# not require them. This removes it from the file in the final step.
+define remove_synopsis
+	(sed -e '/^\.SH "SYNOPSIS"/,/ignore/d' $1 > $1.tmp;\
+	 mv $1.tmp $1)
+endef
+
+define do_install_docs
+	$(print_install)				\
+	if [ ! -d '$(DESTDIR_SQ)$2' ]; then		\
+		$(INSTALL) -d -m 755 '$(DESTDIR_SQ)$2';	\
+	fi;						\
+	$(INSTALL) -m 644 $1 '$(DESTDIR_SQ)$2'
+endef
+
+ifneq ($(findstring $(MAKEFLAGS),s),s)
+ifneq ($(V),1)
+	QUIET_ASCIIDOC	= @echo '  ASCIIDOC  '$@;
+	QUIET_XMLTO	= @echo '  XMLTO    '$@;
+	QUIET_SUBDIR0	= +@subdir=
+	QUIET_SUBDIR1	= ;$(NO_SUBDIR) \
+			   echo '  SUBDIR   ' $$subdir; \
+			  $(MAKE) $(PRINT_DIR) -C $$subdir
+	export V
+endif
+endif

src/.gitignore
@@ -1,0 +1,1 @@
+shelf

src/Makefile
@@ -1,0 +1,38 @@
+# SPDX-License-Identifier: LGPL-2.1
+
+include $(src)/scripts/utils.mk
+
+OBJS =
+OBJS += shelf.o
+OBJS += list.o
+OBJS += dump.o
+
+OBJS := $(OBJS:%.o=$(bdir)/%.o)
+DEPS := $(OBJS:$(bdir)/%.o=$(bdir)/.%.d)
+
+$(bdir)/%.o: %.c
+	$(Q)$(call do_compile)
+
+$(DEPS): $(bdir)/.%.d: %.c
+	$(Q)$(CC) -M -MT $(bdir)/$*.o $(CPPFLAGS) $(CFLAGS) $< > $@
+
+$(OBJS): $(bdir)/%.o : $(bdir)/.%.d
+
+$(OBJS): | $(bdir)
+$(DEPS): | $(bdir)
+
+$(bdir)/shelf: $(OBJS)
+	$(Q)$(do_app_build)
+
+shelf: $(bdir)/shelf
+
+clean:
+	$(Q)$(call do_clean,$(OBJS) .*.d)
+
+dep_includes := $(wildcard $(DEPS))
+
+ifneq ($(dep_includes),)
+  include $(dep_includes)
+endif
+
+.PHONY: shelf clean

commit 38afe8bbb521ac97aa78257185ab928bf85b3cb5

shelf: Fix crash on dumping a symbol not found
When a symbol is not found, the code does not return, and the sym is
still processed even though it is NULL. This causes a crash.

Fixes: 221ca41abb36b ("shelf: Initial commit of the Shell ELF parser")
Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

src/dump.c
@@ -86,8 +86,10 @@ static int dump_symbol(struct ccli *ccli, void *data,
 		return dump_symbol_usage(ccli);
 
 	sym = find_symbol(shelf, argv[0]);
-	if (!sym)
+	if (!sym) {
 		ccli_printf(ccli, "Symbol '%s' not found\n", argv[0]);
+		return 0;
+	}
 
 	shndx = sym_shndx(shelf, sym);
 	shdr = get_shdr(shelf, shndx);